c0b2058a0cc24c396b4070bd222ac3b6b89f736e
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-05-20 20:41:17 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -2,12 +2,10 @@ import sys
 
 inputs = sys.stdin.readline().rstrip()
 
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-a = [-1 for i in range(26)]
-
+a = [-1]*26
 for i in range(len(inputs)):
-    if a[alpha.index(inputs[i])] == -1:
-        a[alpha.index(inputs[i])] = i
+    idx = ord(inputs[i]) - ord('a')
+    if(a[idx]== -1):
+        a[idx] = i
 
-for j in a:
-    print(j, end=' ')
+print(*a)
