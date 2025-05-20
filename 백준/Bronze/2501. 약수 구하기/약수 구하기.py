75d4f851b76d2139400d0caee5e3c52371b78b54
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-05-20 17:43:38 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,9 +1,14 @@
-a, b = map(int, input().split())
-c=[]
-try:
-    for i in range(1, a + 1):
-        if (a % i == 0):
-            c.append(i)
-    print(c[b-1])
-except:
-    print(0)
+import sys
+
+a,b = map(int,sys.stdin.readline().split())
+
+tmp = []
+
+for i in range(1,a+1):
+    if(a%i==0):
+        tmp.append(i)
+
+if(len(tmp) < b):
+    print('0')
+else:
+    print(tmp[b-1])
