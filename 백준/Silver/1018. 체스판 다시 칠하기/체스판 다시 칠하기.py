6d6fbf55bb6e42bbb230506911c5a16cd7e600b2
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-05-23 17:29:51 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,21 +1,27 @@
-a, b = map(int, input().split())
-c = [input() for _ in range(a)]
+import sys
 
-white_start = [['W' if (i + j) % 2 == 0 else 'B' for j in range(8)] for i in range(8)]
-black_start = [['B' if (i + j) % 2 == 0 else 'W' for j in range(8)] for i in range(8)]
+m,n = map(int,sys.stdin.readline().split())
 
-res=100
+board = [sys.stdin.readline().strip() for _ in range(m)]
 
-for i in range(a-7):
-    for j in range(b-7):
-        white=0
-        black=0
-        for k in range(8):
-            for l in range(8):
-                if c[k+i][l+j] == white_start[k][l]:
-                    white+=1
-                if c[k+i][l+j] == black_start[k][l]:
-                    black+=1
-        res = min(res,white,black)
-
-print(res)
+result=[]
+for i in range(m - 7):
+    for j in range(n - 7):
+        cnt1 = 0  # W 체스판
+        cnt2 = 0  # B 체스판
+        for x in range(i,i+8):
+            for y in range(j,j+8):
+                current = board[x][y]
+                if (x + y) % 2 == 0:
+                    if current != 'W':
+                        cnt1 += 1
+                    if current != 'B':
+                        cnt2 += 1
+                else:
+                    if current != 'B':
+                        cnt1 += 1
+                    if current != 'W':
+                        cnt2 += 1
+        result.append(cnt1)
+        result.append(cnt2)
+print(min(result))
