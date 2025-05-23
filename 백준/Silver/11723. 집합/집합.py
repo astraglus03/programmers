96bdba191ef1fe57a28a1ef63319db3c914930d6
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-05-23 11:03:42 +0900

    [Silver V] Title: 집합, Time: 4856 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,28 +1,27 @@
 import sys
 
 a = int(sys.stdin.readline())
-res = set()
-res1 = {i for i in range(1, 21)}
-for i in range(a):
-    b = sys.stdin.readline().split()
+set_list = set()
 
-    if len(b) > 1:
-        tmp = int(b[1])
-    if b[0] == 'add':
-        res.add(tmp)
-    elif b[0] == 'check':
-        if tmp in res:
-            sys.stdout.write('1'+'\n')
+for i in range(a):
+    m = list(map(str, sys.stdin.readline().split()))
+
+    if len(m) == 1:
+        if (m[0] == 'all'):
+            set_list = set(list(range(1, 21)))
         else:
-            sys.stdout.write('0'+'\n')
-    elif b[0] == 'remove':
-        res.discard(tmp)
-    elif b[0] == 'toggle':
-        if tmp in res:
-            res.discard(tmp)
-        else:
-            res.add(tmp)
-    elif b[0] == 'all':
-        res = {i for i in range(1,21)}
-    elif b[0] == 'empty':
-        res.clear()
+            set_list = set()
+
+    else:
+        n = int(m[1])
+        if m[0] == 'add':
+            set_list.add(n)
+        elif m[0] == 'remove':
+            set_list.discard(n)
+        elif m[0] == 'check':
+            print(1 if n in set_list else 0)
+        elif m[0] == 'toggle':
+            if n in set_list:
+                set_list.discard(n)
+            else:
+                set_list.add(n)
