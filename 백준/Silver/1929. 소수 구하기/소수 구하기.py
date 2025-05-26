15b5ef26b541b14749f66766cc09b93d2029292a
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-05-26 09:53:47 +0900

    [Silver III] Title: 소수 구하기, Time: 3420 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,14 @@
 import sys
-a,b = map(int,sys.stdin.readline().split())
-if a<=1:
-    a=2
 
-def checkvalue(n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
+a,b = map(int,sys.stdin.readline().strip().split())
+
+def is_prime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n%i==0:
             return False
+
     return True
 
-while a<=b:
-    if checkvalue(a):
-        print(a)
-    a+=1
+for i in range(a,b+1):
+    if is_prime(i) and i>1:
+        print(i)
