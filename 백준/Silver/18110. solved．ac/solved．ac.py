43678a291ca70e46a8b8d7cbf8311e02b00d1270
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-05-29 17:25:59 +0900

    [Silver IV] Title: solved.ac, Time: 176 ms, Memory: 37164 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -1,20 +1,28 @@
+# 나중에 다시 풀어보기
+
 import sys
 
-
-def judge(n):
+def custom_round(n):
     if n - int(n) >= 0.5:
         return int(n) + 1
     else:
         return int(n)
 
 
-a = int(sys.stdin.readline().rstrip())
-if a == 0:
-    print(0)
-else:
-    b = [int(sys.stdin.readline().rstrip()) for _ in range(a)]
-    b.sort()
-    remove_percent = judge(a * 0.15)
+a = int(sys.stdin.readline().strip())
 
-    b = b[remove_percent:a-remove_percent]
-    print(judge(sum(b) / len(b)))
+if a:
+    stack = []
+    for i in range(a):
+        b = int(sys.stdin.readline().strip())
+        stack.append(b)
+
+    stack.sort()
+    except_count = custom_round(a * 0.15)
+
+    if except_count > 0:
+        print(custom_round(sum(stack[except_count:-except_count]) / len(stack[except_count:-except_count])))
+    else:
+        print(custom_round(sum(stack) / len(stack)))
+else:
+   print(0)
