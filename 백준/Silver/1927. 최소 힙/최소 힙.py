0b53675e886826a785fb14edf50df220121ab86d
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-06-11 09:05:55 +0900

    [Silver II] Title: 최소 힙, Time: 120 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -2,13 +2,14 @@ import sys
 import heapq
 
 a = int(sys.stdin.readline())
-res = []
+heap = []
+
 for i in range(a):
     b = int(sys.stdin.readline())
-    if b> 0:
-        heapq.heappush(res,b)
-    if b == 0:
-        if len(res)>=1:
-            print(heapq.heappop(res))
-        else:
+    if b==0:
+        if len(heap) ==0:
             print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap,b)
