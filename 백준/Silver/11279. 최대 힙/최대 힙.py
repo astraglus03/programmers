d3d1b4dc4168794b5424b3b535bcd83578b6ebc7
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-06-11 09:11:00 +0900

    [Silver II] Title: 최대 힙, Time: 144 ms, Memory: 48580 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -2,13 +2,14 @@ import sys
 import heapq
 
 a = int(sys.stdin.readline())
-res = []
+max_heap = []
+
 for i in range(a):
     b = int(sys.stdin.readline())
-    if b> 0:
-        heapq.heappush(res,(-b,b))
-    if b == 0:
-        if len(res)>=1:
-            print(heapq.heappop(res)[1])
-        else:
+    if b==0:
+        if len(max_heap) ==0:
             print(0)
+        else:
+            print(heapq.heappop(max_heap)[1])
+    else:
+        heapq.heappush(max_heap,(-b,b))
