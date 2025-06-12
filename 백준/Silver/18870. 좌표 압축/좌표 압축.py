bedbd7c9ee8a880d90f33fbc94d0cbed3733dd07
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-06-12 09:29:15 +0900

    [Silver II] Title: 좌표 압축, Time: 1840 ms, Memory: 172328 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,10 +1,14 @@
-a = int(input())
-b = list((map(int, input().split())))
-c = sorted(set(b))
-dict = {}
+import sys
 
-for i in range(0,len(c)):
-    dict[c[i]] = i
+a = int(sys.stdin.readline())
+b = list(map(int, sys.stdin.readline().strip().split()))
+
+set_lst = set(b)
+sorted_set = sorted(set_lst)
+tmp_dict = dict()
+
+for i in range(len(sorted_set)):
+    tmp_dict[sorted_set[i]]= i
 
 for i in b:
-    print(dict[i], end=' ')
+    print(tmp_dict[i],end=' ')
