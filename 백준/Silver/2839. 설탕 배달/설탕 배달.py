54ecb8ac2b5af2144b915c9617084954886fe6c2
Kim Keon Dong <123796383+astraglus03@users.noreply.github.com>
2025-06-23 14:51:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 416 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,16 @@
-a = int(input())
-result = None
-for i in range(0,a):
-    for j in range(0,a):
-        if 5*i+3*j==a:
-            tmp = i+j
-            if result==None:
-                result = tmp
-            if result>tmp:
-                result = tmp
+import sys
 
-if result==None:
+a = int(sys.stdin.readline().strip())
+
+res=[]
+
+for i in range(a//5+1):
+    for j in range(a//3+1):
+        tmp = a - (i*5 + j*3)
+        if tmp==0:
+            res.append(i+j)
+
+if len(res)==0:
     print(-1)
 else:
-    print(result)
+    print(min(res))
